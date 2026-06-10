837db2e53d6d75de7a216bdbd8c1ebf0468b6ffd
kizaru3214 <t.gil0409@gmail.com>
2026-06-10 20:31:47 +0800

    edit for patients

---
+++ patients/edit.php
@@ -0,0 +1,39 @@
+<?php
+    include '../db.php';
+    $id = $_GET["id"];
+    $pat = mysqli_fetch_assoc(mysqli_query($conn, "SELECT * FROM patient WHERE patID = $id"));
+    if($_SERVER ["REQUEST_METHOD"] == "POST"){
+        $fname = $_POST["patFName"];
+        $lname = $_POST["patLName"];
+        $bdate = $_POST["patBDate"];
+        $telno = $_POST["patTelNo"];
+        mysqli_query($conn,"UPDATE patient SET patFName = '$fname', patLName = '$lname', patBDate = '$bdate', patTelNo = '$telno' WHERE patID = $id");
+        header("Location: view.php");
+    }
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Edit Patient</title>
+</head>
+<body>
+    <h2>Edit Patient</h2>
+    <form method = "POST">
+        <label>First Name:</label>
+        <input type = "text" name = "patFName" value = "<?= $pat['patFName'] ?>" required><br><br>
+        
+        <label>Last Name: </label>
+        <input type = "text" name = "patLName" value = "<?= $pat['patLName'] ?>" required><br><br>
+
+        <label>Birth Date: </label>
+        <input type = "text" name = "patBDat" value = "<?=  $pat['patBDate'] ?>" required><br><br>
+
+        <label>Telephone Number: </label>
+        <input type = "Text" name = "patTelNo" value = "<?=  $pat['patTelNo'] ?> required"><br><br>
+    </form>
+    <br><a href = ""
+</body>
+</html>
